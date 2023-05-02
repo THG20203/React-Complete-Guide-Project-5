03353d79bc38bca4246c8543d53fac88762ff9e2
THG20203 <tristanhgriffiths8@yahoo.com>
2023-05-02 21:41:08 +0100

    if statement added for storedUserLoggedInfo

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@ import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
-  /* When app restarts, the app component function runs again, so we could reach out to 
-  localStorage, call getItem(), and search for isLoggedIn and it'll return the items stored there. */
-  const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   /* Where can we utilise USE EFFECT? Would be good to store data somewhere, where it persists 
   the reload, even better than that -> we want to make sure whenever this app does start, 
   we check if that data persisted. Log user in automatically -> so user doesn't need to re 
   enter details. */
+
+  // AFTER we call USE STATE
+  /* When app restarts, the app component function runs again, so we could reach out to 
+  localStorage, call getItem(), and search for isLoggedIn and it'll return the items stored there. */
+  const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+  /* check if storedUserLoggedInInformation is equal to 1 */
+  if (storedUserLoggedInInformation === "1") {
+    /* if it is equal to 1, could call setIsLoggedIn and set as to true. With this, 
+    set the user to login, even without the loginHandler function being triggered, because
+    we know that the user is logged in. */
+    setIsLoggedIn(true);
+  }
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
